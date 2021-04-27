41c01611b853c1830677fdd2cfaf74180346c298
duc410 <ducnguyennhu410@gmail.com>
2021-04-28 01:26:08 +0700

    first commit

---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,42 @@
+import React ,{useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { listProducts } from '../Actions/productActions'
+
+import Product from '../Components/Product'
+import MessageBox from '../Components/MessageBox'
+import LoadingBox from '../Components/LoadingBox'
+
+function HomeScreen() {
+    // const [products,setProducts]=useState([])
+    // const [loading, setLoading] = useState(false)
+    // const [error, setError] = useState(false)
+
+    const dispatch = useDispatch()
+    const productList=useSelector((state)=>state.productList)
+    const { loading , error , products } =productList
+
+    useEffect(() => {
+       dispatch(listProducts())
+    }, [])
+
+    return (
+        <div>
+            {loading? (<LoadingBox></LoadingBox>)
+            :
+            error? (<MessageBox variant="danger">{error}</MessageBox>)
+            :
+            (<div className="row center">
+               {
+                 products.map((product)=>
+                   (
+                    <Product key={product._id} product={product} />
+                   )
+                 )
+                }
+            </div>
+            )}
+        </div>
+    )
+}
+
+export default HomeScreen
